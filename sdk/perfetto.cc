b87d9b12b695cd3c11a7f2d874a1bd6dcfeb23a9
Julian Meder <Julian.Meder@microsoft.com>
2020-08-13 16:10:58 +0200

    Fixing invalid assert where the static state index is erroneously checked against the max allowed data source instances instead of the maximum allowed data sources.

--- sdk/perfetto.cc
+++ sdk/perfetto.cc
@@ -30542,7 +30542,7 @@ void TracingMuxerImpl::UpdateDataSourcesOnAllBackends() {
       if (!backend.producer->connected_)
         continue;
 
-      PERFETTO_DCHECK(rds.static_state->index < kMaxDataSourceInstances);
+      PERFETTO_DCHECK(rds.static_state->index < kMaxDataSources);
       if (backend.producer->registered_data_sources_.test(
               rds.static_state->index))
         continue;
